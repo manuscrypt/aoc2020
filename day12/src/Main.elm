105f09b5d16c818d60e51aeb5dbdf78b6b6bd0c7
Manuel Baumann <manuel.baumann@noser.com>
2020-12-12 13:01:01 +0100

    day12

---
+++ day12/src/Main.elm
@@ -0,0 +1,1005 @@
+module Main exposing (main)
+
+import Html exposing (Html, div, text)
+import List
+import List.Extra as List
+import Parser exposing ((|.), (|=), Parser, int, oneOf, succeed, symbol)
+
+
+type Instruction
+    = Move Direction Int
+    | Rotate Rotation Int
+    | Forward Int
+
+
+type Rotation
+    = L
+    | R
+
+
+type Direction
+    = N
+    | E
+    | S
+    | W
+
+
+type alias Model =
+    { pos : ( Int, Int )
+    , waypoint : ( Int, Int )
+    , heading : Direction
+    }
+
+
+main : Html.Html msg
+main =
+    let
+        instructions =
+            parseInput input
+
+        partA =
+            instructions
+                |> List.foldl applyInstructionA
+                    { pos = ( 0, 0 )
+                    , waypoint = ( 10, -1 )
+                    , heading = E
+                    }
+                |> calcDistance
+
+        partB =
+            instructions
+                |> List.foldl applyInstructionB
+                    { pos = ( 0, 0 )
+                    , waypoint = ( 10, -1 )
+                    , heading = E
+                    }
+                |> calcDistance
+    in
+    div []
+        [ output "part1"
+        , output (String.fromInt partA)
+        , output "part2"
+        , output (String.fromInt partB)
+        ]
+
+
+applyInstructionB : Instruction -> Model -> Model
+applyInstructionB i model =
+    case i of
+        Move dir amt ->
+            { model | waypoint = move amt dir model.waypoint }
+
+        Rotate rot amt ->
+            { model | waypoint = rotateB model.pos rot (amt // 90) model.waypoint }
+
+        Forward amt ->
+            { model | pos = moveB amt model.pos model.waypoint }
+
+
+moveB : Int -> ( Int, Int ) -> ( Int, Int ) -> ( Int, Int )
+moveB amt ( x, y ) ( wx, wy ) =
+    ( x + amt * wx, y + amt * wy )
+
+
+rotateB : ( Int, Int ) -> Rotation -> Int -> ( Int, Int ) -> ( Int, Int )
+rotateB ( x, y ) rot amt ( wx, wy ) =
+    if amt == 0 then
+        ( wx, wy )
+
+    else
+        let
+            _ =
+                ( wx - x, wy - y )
+        in
+        rotateB ( x, y )
+            rot
+            (amt - 1)
+            (case rot of
+                L ->
+                    ( wy, -wx )
+
+                R ->
+                    ( -wy, wx )
+            )
+
+
+applyInstructionA : Instruction -> Model -> Model
+applyInstructionA i model =
+    case i of
+        Move dir amt ->
+            { model | pos = move amt dir model.pos }
+
+        Rotate rot amt ->
+            { model | heading = rotate rot (amt // 90) model.heading }
+
+        Forward amt ->
+            { model | pos = move amt model.heading model.pos }
+
+
+move : Int -> Direction -> ( Int, Int ) -> ( Int, Int )
+move amt dir ( x, y ) =
+    case dir of
+        N ->
+            ( x, y - amt )
+
+        E ->
+            ( x + amt, y )
+
+        S ->
+            ( x, y + amt )
+
+        W ->
+            ( x - amt, y )
+
+
+rotate : Rotation -> Int -> Direction -> Direction
+rotate rot amt dir =
+    if amt == 0 then
+        dir
+
+    else
+        rotate rot
+            (amt - 1)
+            (case dir of
+                N ->
+                    if rot == R then
+                        E
+
+                    else
+                        W
+
+                E ->
+                    if rot == R then
+                        S
+
+                    else
+                        N
+
+                S ->
+                    if rot == R then
+                        W
+
+                    else
+                        E
+
+                W ->
+                    if rot == R then
+                        N
+
+                    else
+                        S
+            )
+
+
+calcDistance : Model -> Int
+calcDistance model =
+    let
+        ( x, y ) =
+            model.pos
+    in
+    abs x + abs y
+
+
+parseInput : String -> List Instruction
+parseInput lines =
+    lines
+        |> String.split "\n"
+        |> List.filterMap (Parser.run parseInstruction >> Result.toMaybe)
+
+
+parseInstruction : Parser Instruction
+parseInstruction =
+    oneOf
+        [ parseMove
+        , parseRotation
+        , succeed Forward |. symbol "F" |= int
+        ]
+
+
+parseRotation : Parser Instruction
+parseRotation =
+    succeed Rotate
+        |= oneOf [ succeed L |. symbol "L", succeed R |. symbol "R" ]
+        |= int
+
+
+parseMove : Parser Instruction
+parseMove =
+    succeed Move
+        |= oneOf
+            [ succeed N |. symbol "N"
+            , succeed E |. symbol "E"
+            , succeed S |. symbol "S"
+            , succeed W |. symbol "W"
+            ]
+        |= int
+
+
+sampleA : String
+sampleA =
+    """F10
+N3
+F7
+R90
+F11"""
+
+
+output : String -> Html msg
+output s =
+    div [] [ text s ]
+
+
+input : String
+input =
+    """N3
+L90
+F63
+W5
+F46
+E3
+F22
+N2
+R90
+F68
+E4
+W3
+R90
+S3
+W4
+R180
+E1
+S5
+F90
+N4
+E3
+N1
+R90
+F74
+R90
+E2
+R90
+W1
+S3
+W4
+F5
+S1
+E5
+S1
+E4
+R90
+E5
+L90
+E4
+R90
+E2
+F57
+N1
+L90
+F59
+R90
+N1
+W3
+S2
+L90
+N3
+E1
+F56
+L180
+S3
+R90
+F88
+E3
+F59
+W1
+N2
+F52
+W4
+F69
+W2
+F10
+W1
+R180
+W1
+R90
+F14
+L90
+W1
+S5
+L90
+S3
+R90
+E3
+F35
+R90
+E3
+S3
+F45
+E2
+R90
+F86
+E1
+E4
+F35
+L180
+S1
+L90
+N2
+F71
+L180
+W3
+S4
+R90
+N5
+F93
+W4
+F74
+L180
+E2
+R180
+F11
+S5
+F28
+S3
+F93
+W2
+N4
+F26
+R90
+S4
+L90
+N1
+L90
+E2
+L90
+F3
+E4
+F43
+R90
+W4
+R90
+E3
+S1
+R180
+L90
+F62
+L90
+E5
+R90
+W3
+L180
+F40
+F20
+N2
+L270
+E1
+F14
+W3
+S5
+R90
+F3
+S2
+L90
+W5
+L270
+W1
+R90
+F11
+R90
+E3
+N1
+E3
+F19
+S5
+L180
+N4
+E2
+R180
+E5
+S2
+W4
+S3
+W1
+F4
+L90
+S2
+W4
+S5
+F21
+L180
+W4
+S3
+L90
+S4
+L90
+E1
+F28
+L180
+S3
+E2
+N3
+L180
+W3
+L90
+F99
+S2
+F63
+E2
+N3
+R90
+E3
+L90
+E5
+L90
+N4
+F39
+R180
+S3
+R90
+N3
+F7
+E3
+S2
+E2
+F98
+S1
+F87
+E1
+S3
+F49
+N1
+W2
+F4
+L270
+F91
+L90
+E1
+S4
+R180
+F43
+S3
+E3
+R90
+F46
+W2
+R90
+W5
+F13
+R180
+F52
+N4
+F28
+N3
+R90
+E5
+S3
+F82
+R90
+W3
+L90
+F33
+S5
+R90
+R90
+S5
+F24
+R90
+N4
+F89
+W1
+S4
+F80
+W3
+L270
+F11
+L90
+W2
+N3
+F18
+R90
+W2
+R90
+E1
+R270
+N3
+R180
+S4
+F36
+S3
+L90
+N2
+L90
+N2
+E1
+F48
+E5
+L180
+S3
+F81
+E4
+L90
+W3
+F31
+E5
+R90
+F66
+S4
+W3
+L90
+E3
+N4
+F85
+L90
+F58
+E5
+L90
+S1
+W3
+F79
+S4
+F60
+N2
+F42
+S3
+W3
+R90
+E1
+N1
+L90
+F15
+E4
+F98
+L90
+R90
+S4
+E1
+F19
+E2
+S4
+R90
+W2
+L180
+N3
+E2
+S3
+F34
+S4
+S4
+L180
+S1
+R90
+S4
+S1
+L90
+E3
+F28
+R90
+W1
+N2
+E5
+F48
+E4
+S1
+W2
+F95
+W2
+N2
+L90
+E2
+L90
+W3
+S2
+L270
+W4
+L90
+N4
+R90
+E4
+R270
+W4
+F6
+W2
+N1
+E1
+F19
+W2
+N1
+F54
+W2
+L90
+S1
+L90
+F80
+E1
+S5
+E5
+F80
+R90
+L270
+E4
+F93
+N4
+E5
+S1
+E1
+R90
+F63
+N3
+R90
+E1
+N2
+L90
+W5
+R90
+R270
+N1
+E4
+L180
+E4
+F19
+L90
+F27
+W2
+S2
+W5
+S1
+F54
+S4
+R90
+F85
+W2
+F13
+R90
+F73
+S5
+E2
+S2
+F12
+W5
+F23
+N1
+E1
+F38
+N2
+W2
+N3
+E2
+L270
+F7
+L90
+S3
+L90
+S3
+F86
+E5
+R90
+E1
+F52
+L180
+S4
+L180
+W4
+F41
+R90
+E3
+F70
+R270
+N3
+F32
+S2
+E5
+R180
+F20
+W3
+F54
+E2
+F34
+F61
+S5
+W1
+L90
+S5
+N5
+W2
+R180
+W2
+L90
+E5
+S4
+L90
+S4
+L180
+F84
+S1
+W1
+L90
+F92
+F46
+N1
+F22
+F24
+L90
+N5
+W4
+R270
+F79
+N1
+W1
+F68
+R90
+W5
+R180
+N5
+L90
+L180
+S1
+W4
+N1
+L180
+S1
+N4
+E4
+R90
+E1
+E4
+F58
+S4
+E5
+F49
+N1
+E2
+S4
+L90
+W2
+F67
+E2
+N5
+W1
+L90
+E5
+F82
+N5
+F91
+W5
+R90
+F17
+W5
+S2
+R90
+N2
+R90
+N5
+E4
+L90
+N1
+F26
+N3
+E3
+F19
+L270
+R90
+E3
+F21
+L180
+S4
+F50
+S4
+W2
+F56
+F49
+N2
+E3
+R180
+E4
+F5
+F17
+E2
+R90
+N3
+F96
+L180
+E4
+F64
+W5
+R90
+W5
+S5
+F92
+E5
+F10
+N1
+W1
+F94
+R90
+W4
+F22
+S1
+W4
+F38
+W1
+F17
+E3
+L90
+F3
+S1
+L90
+F27
+W4
+F31
+S5
+W4
+N2
+E5
+F44
+W2
+E4
+F54
+L180
+E5
+L90
+N1
+E5
+N4
+L180
+L270
+W3
+F80
+S2
+F49
+E4
+F46
+E2
+E5
+L270
+F12
+F63
+L90
+N2
+E5
+N3
+F85
+R270
+S3
+F71
+N4
+E5
+F36
+N5
+F23
+L90
+N2
+E3
+F93
+S5
+F1
+S2
+F29
+L90
+F17
+R180
+S4
+R90
+E2
+S3
+W5
+R90
+S3
+R90
+W4
+F62
+L180
+S4
+L90
+N2
+F46
+N3
+R180
+E1
+R90
+F73
+S5
+F12
+L180
+F47
+L90
+F79
+N4
+R270
+W3
+N1
+W1
+N3
+F63
+S2
+F50
+R90
+F30
+N3
+F7
+N4
+L90
+S4
+N1
+E5
+S5
+F9
+L90
+L90
+F7
+N1
+R90
+F52
+E3
+L90
+N3
+F50
+L90
+F83
+E3
+F74
+L90
+N1
+L90
+F4
+N1
+F28
+E4
+F9
+E4
+S2
+W4
+L270
+S1
+W4
+F23
+E1
+F52
+E1
+L180
+E2
+N5
+L90
+W5
+L90
+S1
+E3
+R90
+E4
+L90
+S1
+W2
+N4
+W1
+S4
+E2
+L90
+E5
+S2
+L180
+F91
+N5
+W4
+N5
+F14
+S5
+R90
+S5
+L90
+F78
+N2
+W3
+R90
+F17
+N5
+W1
+F53
+W2
+F33
+R90
+E2
+F15
+L90
+E5
+F77
+L90
+S1
+F33"""
